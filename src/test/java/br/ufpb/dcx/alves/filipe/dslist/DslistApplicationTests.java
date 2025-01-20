f97dac985f9ad6248eb7a36697b8fd56c0c00f20
Filipealves0 <filipe.alves@dcx.ufpb.br>
2025-01-20 10:49:50 -0300

    Projeto criado

---
+++ src/test/java/br/ufpb/dcx/alves/filipe/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.alves.filipe.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
